160a30c2665493edcec8012983c1e705f6d0214b
Mehul Chattopadhyay <mehul.chattopadhyay@tuteck.com>
2024-01-22 15:18:24 +0530

    Adding models folder and setting up routes

--- server.js
+++ server.js
@@ -3,16 +3,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+//Import all routes
+const routes = require("./routes")
+
 const server = express();
-
-
 server.use(cors());
 server.use(express.json()); 
+
 
 
 server.get("/", (req, res) => {
   res.send("Hello, World!");
 });
+
+server.use("/api",routes)
 
 
 const PORT = process.env.PORT || 3000;
