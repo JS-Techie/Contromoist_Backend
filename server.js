242525a5009e4b9536d9155ada615d9ca6228895
Mehul Chattopadhyay <mehul.chattopadhyay@tuteck.com>
2024-01-22 15:27:47 +0530

    Routes and Controllers naming and files

--- server.js
+++ server.js
@@ -11,13 +11,11 @@ server.use(cors());
 server.use(express.json()); 
 
 
-
 server.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
 server.use("/api",routes)
-
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
