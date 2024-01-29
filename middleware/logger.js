631773118d88d8f2774b2e014a79a8ba8a696348
Mehul Chattopadhyay <mehul.chattopadhyay@tuteck.com>
2024-01-29 10:01:16 +0530

    Writing logs to log files to later upload them to s3

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,12 @@
 const morgan = require("morgan");
-const {print} = require("../utils")
+const { print } = require("../utils");
+const fs = require('fs');
+const path = require('path');
 
 morgan.token("logger", (req, res) => {
-    print(JSON.stringify({
+    const formattedDate = getFormattedDate();
+    
+    const log = JSON.stringify({
         TIMESTAMP: new Date().toISOString(),
         METHOD: req.method,
         API_URL: req.url,
@@ -10,7 +14,18 @@ morgan.token("logger", (req, res) => {
         HEADERS: req.headers,
         USER: req.user ? req.user.id : "Not authenticated",
         REQUEST_BODY: req.method !== "GET" ? req.body : null,
-    }),"LOG");
+    });
+
+    print(log, "LOG");
+
+    const fileName = path.join("./docs", `${formattedDate}.txt`);
+
+    if (!fs.existsSync(fileName)) {
+        // If the file doesn't exist, create it
+        fs.writeFileSync(fileName, '');
+    }
+
+    fs.appendFileSync(fileName, log);
 
     return "";
 });
@@ -18,3 +33,12 @@ morgan.token("logger", (req, res) => {
 module.exports = {
     logger: morgan(":logger")
 };
+
+function getFormattedDate() {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const yy = String(today.getFullYear()).slice(-2);
+
+    return `${dd}-${mm}-${yy}`;
+}
