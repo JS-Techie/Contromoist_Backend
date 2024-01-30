d59d5368b5eb8ee6881989b1f592ce09fd9cb0cf
Mehul Chattopadhyay <mehul.chattopadhyay@tuteck.com>
2024-01-30 11:45:49 +0530

    Routes and controllers for TMS

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+const travelController = require("./travel")
+
+
+module.exports = {
+    travelController
+}
---
+++ controllers/travel.js
@@ -0,0 +1,30 @@
+class TravelController {
+
+    getAllRequisitions = async (req, res, next) => {
+
+    }
+
+    getRequisitionById = async (req, res, next) => {
+
+    }
+
+    getRequisitionByProjectId = async (req, res, next) => {
+
+    }
+
+    createRequisition = async (req, res, next) => {
+
+    }
+
+    editRequisition = async (req, res, next) => {
+
+    }
+    
+    deleteRequisition = async (req, res, next) => {
+
+    }
+
+
+}
+
+module.exports = new TravelController()
