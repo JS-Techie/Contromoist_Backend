d317d832c8c5bfa3644b2cd3e828cea6915032f8
Mehul Chattopadhyay <mehul.chattopadhyay@tuteck.com>
2024-02-01 12:32:01 +0530

    TMS complete APIs, unit testing now

--- controllers/travel.js
+++ controllers/travel.js
@@ -1,48 +1,48 @@
-const { Response, print, logType } = require('../utils');
-const db = require("../models");
+const {
+    Response,
+    print,
+    logType
+} = require('../utils');
+const db = require('../models');
+const {
+    Project
+} = require('../services');
 
-const Travel = db.Travel
+const {
+    Travel,
+    TravelDetails
+} = db;
+const {
+    Sequelize
+} = require('sequelize');
+
+const projectService = new Project();
 
 class TravelController {
-    getAllRequisitions = async (req, res, next) => {
+
+    async getAllRequisitions(req, res, next) {
         try {
-            // Need to know how to check the user type
             if (!req.user || !req.user.id) {
                 return Response.errorUnauthorized()(res);
             }
 
-            const isAdmin = req.user && req.user.isAdmin;
+            const isAdmin = req.user.isAdmin;
 
-            let requisitions;
+            const [data, ok] = await projectService.fetchAll(req.user.id, isAdmin)
 
-            if (isAdmin) {
-
-                requisitions = await Travel.find();
-            } else {
-
-                if (!req.user.id) {
-                    return Response.errorUnauthorized()(res);
-                }
-
-                requisitions = await Travel.find({ resource: req.user.id });
+            if (!ok) {
+                return Response.errorGeneric([], data)(res);
             }
 
+            return Response.successFetch(data)(res);
 
-            if (!requisitions || requisitions.length === 0) {
-                print("REQUSITIONS FETCHED BUT EMPTY", logType.warning)
-                return Response.errorNotFound()(res);
-            }
-
-            print("REQUSITIONS FETCHED", logType.success)
-
-            return Response.successFetch(requisitions)(res);
         } catch (error) {
-            print(str(error), logType.error)
+            print(String(error), logType.error);
             return Response.errorGeneric([], error.message)(res);
         }
-    };
+    }
 
-    getRequisitionById = async (req, res, next) => {
+    async getRequisitionById(req, res, next) {
         try {
             if (!req.user || !req.user.id) {
                 return Response.errorUnauthorized()(res);
@@ -51,117 +51,165 @@ class TravelController {
             const requisitionId = req.params.id;
 
             if (!requisitionId) {
-                return Response.errorGeneric([], "Requisition ID Empty", "This requsition ID doesn't exist / is invalid!")(res)
-            }
-            // Need to know how to check the user type
-            const isAdmin = req.user && req.user.isAdmin;
-            let requisition;
-
-            if (isAdmin) {
-
-
-                requisition = await Travel.findOne({ id: requisitionId });
-            } else {
-
-                if (!req.user.id) {
-                    return Response.errorUnauthorized()(res);
-                }
-
-                requisition = await Travel.findOne({ id: requisitionId, resource: req.user.id });
+                return Response.errorGeneric([], 'Requisition ID Empty', 'This requisition ID doesn\'t exist or is invalid!')(res);
             }
 
-            if (!requisition) {
-                print("REQUISITION FETCHED BUT EMPTY", logType.warning)
-                return Response.errorNotFound()(res);
+            const isAdmin = req.user.isAdmin;
+
+            const [data, ok] = await projectService.fetchById(requisitionId, req.user.id, isAdmin)
+
+            if (!ok) {
+                return Response.errorGeneric([], data)(res);
             }
 
-            print("REQUISITION FETCHED", logType.success)
-
-
-            return Response.successFetch(requisition)(res);
-
+            return Response.successFetch(data)(res);
         } catch (error) {
-            print(str(error), logType.error)
+            print(String(error), logType.error);
             return Response.errorGeneric([], error.message)(res);
         }
     }
 
-    getRequisitionByProjectId = async (req, res, next) => {
+    async getRequisitionByProjectId(req, res, next) {
         try {
-            const { project } = req.params;
+            const {
+                project
+            } = req.params;
 
-            // Check if the user is unauthorized
             if (!req.user || !req.user.id) {
                 return Response.errorUnauthorized()(res);
             }
 
-            // Check if the user is an admin
             const isAdmin = req.user.isAdmin;
 
-            // Fetch requisitions based on the project ID and user ID (admin fetches all)
-            const requisitions = isAdmin
-                ? await Travel.find({ project })
-                : await Travel.find({ project, resource: req.user.id });
+            const [data, ok] = await projectService.fetchByProjectId(req.user.id, project, isAdmin)
 
-            // Check if requisitions are empty
-            if (!requisitions || requisitions.length === 0) {
-                print("REQUISITIONS FETCHED BUT EMPTY BY PROJECT ID", logType.warning)
+            if (!ok) {
+                return Response.errorGeneric([], data)(res);
+            }
+            return Response.successFetch(data)(res);
+
+        } catch (error) {
+            print(String(error), logType.error);
+            return Response.errorGeneric([], error.message)(res);
+        }
+    }
+
+    async createRequisition(req, res, next) {
+        const transaction = await db.sequelize.transaction();
+
+        try {
+            if (!req.user || !req.user.id) {
+                return Response.errorUnauthorized()(res);
+            }
+
+            const {
+                project,
+                details
+            } = req.body;
+
+            if (!project) {
+                return Response.errorGeneric([], 'Valid project ID not present', 'Please select a project to enter a new requisition!')(res);
+            }
+
+            const resourceAssignedToProject = await projectService.resourceAssignedToProject();
+
+            if (!resourceAssignedToProject) {
+                print(`USER ${req.user.id} WANTED TO CREATE NEW REQUISITION BUT UNAUTHORIZED`, logType.error);
+                return Response.errorGeneric([], 'Not authorized for this project', 'You are not assigned to this project!')(res);
+            }
+
+            const [data, ok] = await projectService.createRequisition(project, details, req.user.id)
+
+            if (!ok) {
+                return Response.errorGeneric([], data)(res);
+            }
+            return Response.successCreate(data)(res);
+
+        } catch (error) {
+            await transaction.rollback();
+            print(String(error), logType.error);
+            return Response.errorGeneric([], error.message)(res);
+        }
+    }
+
+    async editRequisition(req, res, next) {
+        const transaction = await db.sequelize.transaction();
+
+        try {
+            if (!req.user || !req.user.id) {
+                return Response.errorUnauthorized()(res);
+            }
+
+            const requisitionId = req.params.id;
+
+            if (!requisitionId) {
+                return Response.errorGeneric([], 'Requisition ID Empty', 'This requisition ID doesn\'t exist or is invalid!')(res);
+            }
+
+            const requisition = await projectService.fetchById(requisitionId, req.user.id, req.user.isAdmin)
+            if (!requisition) {
+                print('REQUISITION FETCHED BUT EMPTY', logType.warning);
                 return Response.errorNotFound()(res);
             }
-            print("REQUISITIONS FETCHED", logType.success)
-            return Response.successFetch(requisitions)(res);
+
+            const {
+                project,
+                details
+            } = req.body
+
+            if (!project) {
+                return Response.errorGeneric([], 'Valid project ID not present', 'Please select a project to edit the requisition!')(res);
+            }
+
+            const resourceAssignedToProject = await projectService.resourceAssignedToProject();
+
+            if (!resourceAssignedToProject) {
+                print(`USER ${req.user.id} WANTED TO EDIT REQUISITION BUT UNAUTHORIZED`, logType.error);
+                return Response.errorGeneric([], 'Not authorized for this project', 'You are not assigned to this project!')(res);
+            }
+
+            if (!details || details.length === 0) {
+                return Response.errorGeneric([], 'Details array is required and should not be empty', 'Please provide valid details for editing the requisition!')(res);
+            }
+
+            const [data, ok] = await projectService.editRequisition(requisitionId, req.user.id, details, project)
+
+            if (!ok) {
+                return Response.errorGeneric([], data)(res);
+            }
+
+            print(`USER ${req.user.id} EDITED REQUISITION`, logType.success);
+
+            return Response.successUpdate(data)(res);
+
         } catch (error) {
-            print(str(error), logType.error)
-            return Response.errorGeneric([], error.message)(res);
-        }
-    };
-
-
-
-    createRequisition = async (req, res, next) => {
-        try {
-            const newRequisitionData = { ...req.body, userId: req.userId };
-            const newRequisition = await Travel.create(newRequisitionData);
-            return Response.successCreate(newRequisition)(res);
-        } catch (error) {
+            await transaction.rollback();
+            print(String(error), logType.error);
             return Response.errorGeneric([], error.message)(res);
         }
     }
 
-    editRequisition = async (req, res, next) => {
+    async deleteRequisition(req, res, next) {
+        const transaction = await db.sequelize.transaction();
+
         try {
             const requisitionId = req.params.id;
-            const updatedRequisitionData = req.body;
-            const updatedRequisition = await Travel.findOneAndUpdate(
-                { _id: requisitionId, userId: req.userId },
-                updatedRequisitionData,
-                { new: true }
-            );
 
-            if (!updatedRequisition) {
-                return Response.errorNotFound([], "Requisition not found")(res);
+            const [deletedRequisition, ok] = await projectService.deleteRequisition(requisitionId);
+
+            if (!ok) {
+                await transaction.rollback();
+                return Response.errorGeneric([], deletedRequisition)(res);
             }
 
-            return Response.editSuccess(updatedRequisition)(res);
-        } catch (error) {
-            return Response.errorGeneric([], error.message)(res);
-        }
-    }
-
-    deleteRequisition = async (req, res, next) => {
-        try {
-            const requisitionId = req.params.id;
-            const deletedRequisition = await Travel.findOneAndDelete({ _id: requisitionId, userId: req.userId });
-
-            if (!deletedRequisition) {
-                return Response.errorNotFound([], "Requisition not found")(res);
-            }
-
+            await transaction.commit();
             return Response.deleteSuccess(deletedRequisition)(res);
         } catch (error) {
+            await transaction.rollback();
             return Response.errorGeneric([], error.message)(res);
         }
     }
+
 }
 
 module.exports = new TravelController();
---
+++ services/project.js
@@ -0,0 +1,250 @@
+const {
+    print,
+    logType
+} = require("../utils");
+const db = require("../models");
+
+const {
+    ProjectResource,
+    Travel,
+    TravelDetails
+} = db;
+
+class Project {
+
+    async fetchAll(resource, isAdmin) {
+        try {
+            const requisitions = await Travel.findAll({
+                include: [{
+                    model: TravelDetails,
+                    where: isAdmin ? {} : {
+                        travel_id: db.Sequelize.col('Travel.id')
+                    },
+                    required: true,
+                }],
+                where: isAdmin ? {} : {
+                    resource
+                },
+            });
+
+            if (!requisitions || requisitions.length === 0) {
+                print('REQUSITIONS FETCHED BUT EMPTY', logType.warning);
+                return ([], true);
+            }
+
+            print('REQUSITIONS FETCHED', logType.success);
+            return (requisitions, true);
+
+        } catch (error) {
+            print(String(error), logType.error);
+            return (String(error), false);
+        }
+    }
+
+    async fetchById(requisitionId, resource, isAdmin) {
+        try {
+            const requisition = await Travel.findOne({
+                where: isAdmin ? {
+                    id: requisitionId
+                } : {
+                    id: requisitionId,
+                    resource: resource
+                },
+                include: [{
+                    model: TravelDetails,
+                    where: {
+                        travel_id: db.Sequelize.col('Travel.id')
+                    },
+                    required: true,
+                }],
+            });
+
+            if (!requisition) {
+                print('REQUISITION FETCHED BUT EMPTY', logType.warning);
+                return ([], true);
+            }
+
+            print('REQUISITION FETCHED', logType.success);
+            return (requisition, true);
+
+        } catch (error) {
+            print(String(error), logType.error);
+            return (String(error), false);
+        }
+    }
+
+    async fetchByProjectId(resource, project, isAdmin) {
+        try {
+            const requisitions = await Travel.findAll({
+                where: isAdmin ? {
+                    project
+                } : {
+                    project,
+                    resource
+                },
+                include: [{
+                    model: TravelDetails,
+                    where: {
+                        travel_id: db.Sequelize.col('Travel.id')
+                    },
+                    required: true,
+                }],
+            });
+
+            if (!requisitions || requisitions.length === 0) {
+                print('REQUISITIONS FETCHED BUT EMPTY BY PROJECT ID', logType.warning);
+                return ([], true);
+            }
+            print('REQUISITIONS FETCHED BY PROJECT ID', logType.success);
+            return (requisitions, true);
+
+        } catch (error) {
+            print(String(error), logType.error);
+            return (String(error), false);
+        }
+    }
+
+    async createRequisition(project, details, resource) {
+        const transaction = await db.sequelize.transaction();
+
+        try {
+            const createdTravel = await Travel.create({
+                project
+            }, {
+                transaction
+            });
+
+            const travelDetailsRecords = details.map((detail) => ({
+                location: detail.location,
+                reason: detail.reason,
+                amount: detail.amount,
+                travel_id: createdTravel.id,
+                created_by: resource,
+                updated_by: resource,
+                is_active: true,
+                is_approved: false
+            }));
+
+            await TravelDetails.bulkCreate(travelDetailsRecords, {
+                transaction
+            });
+
+            await transaction.commit();
+
+            print(`USER ${resource} CREATED NEW REQUISITION`, logType.success);
+
+            return (createdTravel, true);
+
+        } catch (error) {
+            await transaction.rollback();
+            print(String(error), logType.error);
+            return (String(error), false);
+        }
+    }
+
+    async editRequisition(requisitionId, resource, details, project) {
+        const transaction = await db.sequelize.transaction();
+
+        try {
+            const recordIds = details.map((detail) => detail.id);
+
+            const travelRecords = details.map((detail) => ({
+                id: detail.id,
+                location: detail.location,
+                reason: detail.reason,
+                amount: detail.amount,
+                project,
+                created_by: resource,
+                travel_id: requisitionId,
+            }));
+
+            await Travel.bulkUpdate(travelRecords, {
+                updateOnDuplicate: ['location', 'reason', 'amount']
+            });
+
+            await transaction.commit();
+
+            return (recordIds, true);
+
+        } catch (error) {
+            print(String(error), logType.error);
+            return (String(error), false);
+        }
+    }
+
+    async deleteRequisition(requisitionId, resource) {
+        const transaction = await db.sequelize.transaction();
+
+        try {
+            const updatedRequisition = await Travel.findOne({
+                where: {
+                    id: requisitionId,
+                    resource: resource
+                },
+                include: [{
+                    model: TravelDetails,
+                    where: {
+                        travel_id: db.Sequelize.col('Travel.id')
+                    },
+                    required: true,
+                }],
+                transaction,
+            });
+
+            if (!updatedRequisition) {
+                print('REQUISITION NOT FOUND FOR DELETION', logType.warning);
+                await transaction.rollback();
+                return ([], false);
+            }
+
+            await Travel.update({
+                is_active: false
+            }, {
+                where: {
+                    id: requisitionId
+                },
+                transaction
+            });
+
+            await TravelDetails.update({
+                is_active: false
+            }, {
+                where: {
+                    travel_id: requisitionId
+                },
+                transaction
+            });
+
+            await transaction.commit();
+
+            print(`REQUISITION DELETED: ${requisitionId}`, logType.success);
+            return (updatedRequisition, true);
+        } catch (error) {
+            await transaction.rollback();
+            print(String(error), logType.error);
+            return (String(error), false);
+        }
+    };
+
+
+    async resourceAssignedToProject(project, resource) {
+        try {
+            const assigned = await ProjectResource.findOne({
+                project,
+                resource
+            });
+
+            if (!assigned) {
+                print(`USER ${resource} IS NOT ASSIGNED TO PROJECT ${project}`, logType.warning);
+                return (false, true);
+            }
+            return (true, true);
+
+        } catch (error) {
+            print(String(error), logType.error);
+            return (String(error), false);
+        }
+    }
+}
+
+module.exports = Project;
